b01e82e0cb1ed579a193b28109f61485bffb7773
dinox124 <40330463+dinox124@users.noreply.github.com>
2019-05-26 18:15:27 +0530

    first commit

---
+++ app/src/main/java/com/gsolution/gpsrealtimelocation/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.gsolution.gpsrealtimelocation;
+
+/**
+ * Created by sonbu on 3/16/2018.
+ */
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {}
+
